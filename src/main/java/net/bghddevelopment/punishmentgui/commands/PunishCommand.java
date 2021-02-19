41ec1e33c8de7d389df443f9b4d9f6a6d3f56002
BGHDDevelopment <ceo@bghddevelopment.com>
2021-02-18 18:10:10 -0800

    Fixes

--- src/main/java/net/bghddevelopment/punishmentgui/commands/PunishCommand.java
+++ src/main/java/net/bghddevelopment/punishmentgui/commands/PunishCommand.java
@@ -2,6 +2,7 @@ package net.bghddevelopment.punishmentgui.commands;
 
 import net.bghddevelopment.punishmentgui.language.Language;
 import net.bghddevelopment.punishmentgui.menu.handler.CustomMenu;
+import net.bghddevelopment.punishmentgui.utils.Color;
 import net.bghddevelopment.punishmentgui.utils.Tasks;
 import net.bghddevelopment.punishmentgui.utils.Utilities;
 import net.bghddevelopment.punishmentgui.utils.command.BaseCommand;
@@ -18,6 +19,10 @@ public class PunishCommand extends BaseCommand {
     public void onCommand(CommandArgs command) {
         Player player = command.getPlayer();
         String[] args = command.getArgs();
+        if (!(command.getSender() instanceof Player)) {
+            command.getSender().sendMessage(Color.translate("This can only be used in-game!"));
+            return;
+        }
         if (!player.hasPermission("punish.use")) {
             return;
         }
@@ -28,22 +33,22 @@ public class PunishCommand extends BaseCommand {
         if (args.length == 1) {
             plugin.getBannedManager().add(player.getUniqueId(), args[0]);
         }
-        if(plugin.getBannedManager().get(player.getUniqueId()).length() > 16){
+        if (plugin.getBannedManager().get(player.getUniqueId()).length() > 16) {
             player.sendMessage(plugin.getPlaceholderAPI().translate(player, Language.PUNISH_NAME_ERROR.toString()));
             return;
         }
-            String menu = plugin.getSettingsFile().getString("Command").replace("{openmenu:", "").replace("}", "").toLowerCase();
-            CustomMenu customMenu = plugin.getCoreHandler().getCustomMenuData().get(menu);
-            if (menu != null) {
-                Tasks.run(plugin, () -> {
-                    player.closeInventory();
-                    customMenu.getMenu().open(player);
-                    return;
-                });
-            } else {
-                Utilities.log("&c[MenuLog-1] &eThere is no menu with name &e&n" + plugin.getSettingsFile().getString("Command") + "&b &eto open for &b" + player.getName() + "&e. &c&oPlease check your configurations.");
+        String menu = plugin.getSettingsFile().getString("Command").replace("{openmenu:", "").replace("}", "").toLowerCase();
+        CustomMenu customMenu = plugin.getCoreHandler().getCustomMenuData().get(menu);
+        if (menu != null) {
+            Tasks.run(plugin, () -> {
+                player.closeInventory();
+                customMenu.getMenu().open(player);
                 return;
-            }
-            player.updateInventory();
+            });
+        } else {
+            Utilities.log("&c[MenuLog-1] &eThere is no menu with name &e&n" + plugin.getSettingsFile().getString("Command") + "&b &eto open for &b" + player.getName() + "&e. &c&oPlease check your configurations.");
+            return;
         }
+        player.updateInventory();
     }
+}
--- src/main/java/net/bghddevelopment/punishmentgui/commands/PunishReloadCommand.java
+++ src/main/java/net/bghddevelopment/punishmentgui/commands/PunishReloadCommand.java
@@ -4,19 +4,27 @@ import net.bghddevelopment.punishmentgui.utils.command.BaseCommand;
 import net.bghddevelopment.punishmentgui.utils.command.Command;
 import net.bghddevelopment.punishmentgui.utils.command.CommandArgs;
 import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 public class PunishReloadCommand extends BaseCommand {
 
     @Command(name = "punishreload", aliases = "preload", permission = "punish.admin")
     public void onCommand(CommandArgs command) {
-        Player player = command.getPlayer();
-        if (!player.hasPermission("punish.admin")) {
-            return;
+        if (!(command.getSender() instanceof Player)) {
+            plugin.getSettingsFile().load();
+            plugin.getLanguageFile().load();
+            plugin.getCoreHandler().setupCustomMenuData();
+            command.getSender().sendMessage(ChatColor.GREEN + "PunishmentGUI reloaded!");
+        } else {
+            Player player = command.getPlayer();
+            if (!player.hasPermission("punish.admin")) {
+                return;
+            }
+            plugin.getSettingsFile().load();
+            plugin.getLanguageFile().load();
+            plugin.getCoreHandler().setupCustomMenuData();
+            player.sendMessage(ChatColor.GREEN + "PunishmentGUI reloaded!");
         }
-        plugin.getSettingsFile().load();
-        plugin.getLanguageFile().load();
-        plugin.getCoreHandler().setupCustomMenuData();
-        player.sendMessage(ChatColor.GREEN + "PunishmentGUI reloaded!");
     }
 }
