137eba85f5de621c802f432a68a67ba038f53c7f
Zachary Baldwin <zacharybaldwin904@gmail.com>
2021-12-05 22:26:05 -0600

    Add Skulls support

--- src/main/java/net/bghddevelopment/punishmentgui/menu/handler/ConfigItem.java
+++ src/main/java/net/bghddevelopment/punishmentgui/menu/handler/ConfigItem.java
@@ -1,5 +1,7 @@
 package net.bghddevelopment.punishmentgui.menu.handler;
 
+import ca.tweetzy.skulls.api.SkullsAPI;
+import ca.tweetzy.skulls.impl.Skull;
 import lombok.Getter;
 import lombok.Setter;
 import me.arcaniax.hdb.api.HeadDatabaseAPI;
@@ -32,7 +34,7 @@ public class ConfigItem {
     private Material material;
     private int durability, slot;
     private List<String> lore;
-    private boolean commandEnabled, closeMenu, headDatabase;
+    private boolean commandEnabled, closeMenu, headDatabase, skulls;
     private boolean glow = false;
     private String message;
     private boolean messageEnabled;
@@ -54,6 +56,11 @@ public class ConfigItem {
         } else {
             this.headDatabase = false;
         }
+        if (this.configuration.contains(this.path + ".skulls")) {
+            this.skulls = this.configuration.getBoolean(this.path + ".skulls", false);
+        } else {
+            this.skulls = false;
+        }
         this.skullOwner = this.configuration.getString(this.path + ".skullOwner");
         this.lore = this.configuration.getStringList(this.path + ".lore");
         this.slot = this.configuration.getInt(this.path + ".slot") - 1;
@@ -73,6 +80,24 @@ public class ConfigItem {
         if (headDatabase) {
             HeadDatabaseAPI api = new HeadDatabaseAPI();
             ItemBuilder item = new ItemBuilder(api.getItemHead(this.skullOwner));
+            if (glow) {
+                if (Bukkit.getVersion().contains("1.7")) {
+                    item.addEnchant(Enchantment.ARROW_DAMAGE, 1);
+                } else if (Bukkit.getVersion().contains("1.13") || Bukkit.getVersion().contains("1.14") || Bukkit.getVersion().contains("1.15") || Bukkit.getVersion().contains("1.16")) {
+                    item.addEnchant(PunishGUI.getInstance().getGlow(), 1);
+                } else {
+                    item.addEnchant(Enchantment.ARROW_DAMAGE, 1);
+                    ItemMeta itemMeta = item.toItemStack().getItemMeta();
+                    itemMeta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+                    item.toItemStack().setItemMeta(itemMeta);
+                }
+            }
+            item.setName(this.name);
+            item.setLore(this.lore);
+            return item.toItemStack();
+        } else if (skulls) {
+            Skull api = SkullsAPI.getSkull(Integer.parseInt(this.skullOwner));
+            ItemBuilder item = new ItemBuilder(api.getItemStack());
             if (glow) {
                 if (Bukkit.getVersion().contains("1.7")) {
                     item.addEnchant(Enchantment.ARROW_DAMAGE, 1);
